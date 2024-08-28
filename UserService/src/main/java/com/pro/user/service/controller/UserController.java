e26ceee7094cbae7fe7f26300115755f86fe74d4
bhaskarsharma516 <bhaskarsharma516@gmail.com>
2024-08-28 18:21:17 +0530

    retry impl in user service

--- UserService/src/main/java/com/pro/user/service/controller/UserController.java
+++ UserService/src/main/java/com/pro/user/service/controller/UserController.java
@@ -16,6 +16,7 @@ import com.pro.user.service.entities.User;
 import com.pro.user.service.services.UserService;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.Builder;
 
 @RestController
@@ -34,6 +35,7 @@ public class UserController {
 
 	@GetMapping("/{userId}")
 	@CircuitBreaker(name="ratingHotelBreaker",fallbackMethod = "ratingHotelFallbackMethod")
+	@Retry(name="ratingHotelBreaker",fallbackMethod = "ratingHotelFallbackMethod")
 	public ResponseEntity<User> getSingleUser(@PathVariable String userId){
 		var userData=userService.getUser(userId);
 		return  ResponseEntity.ok(userData);
